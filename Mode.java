4804564dcd858f1c9d93d2eaec75b6aca8d62679
Magesh balram <138652814+Magesh2004@users.noreply.github.com>
2024-05-16 01:18:13 +0530

    Fix : added commets

--- Mode.java
+++ Mode.java
@@ -2,11 +2,13 @@ public class Mode {
     public static void main(String[] args) {
         int [] a = {1,2,3,4,3,5,5};
         int i,j,count=0,max=0,maxValue=0;
+        //Finding the count of each element
         for(i =0;i<a.length;i++){
             for(j =0;j<a.length;j++){
                 if(a[i]==a[j])
                 count++;
             }
+            //Finding the maximum number of time a element occuren
             if (count>max) {
                 max= count;
                 maxValue=a[i];
