2abd6888426db6e3fc257a139e2a124eb7cdef4a
Magesh balram <mageshbalram4@gmail.com>
2024-05-16 01:09:58 +0530

    feat : mode

---
+++ Mode.java
@@ -0,0 +1,17 @@
+public class Mode {
+    public static void main(String[] args) {
+        int [] a = {1,2,3,4,3,5,5};
+        int i,j,count=0,max=0,maxValue=0;
+        for(i =0;i<a.length;i++){
+            for(j =0;j<a.length;j++){
+                if(a[i]==a[j])
+                count++;
+            }
+            if (count>max) {
+                max= count;
+                maxValue=a[i];
+            }
+        }
+        System.out.println("Mode of the array is :" + maxValue);
+    }
+}
