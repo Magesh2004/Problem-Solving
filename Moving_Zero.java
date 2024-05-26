586bb6dba9a9f7f1e5d0bdbc9e0920f4c8e7ca7c
Magesh balram <mageshbalram4@gmail.com>
2024-05-26 12:45:56 +0530

    !Fix : fixing an error

--- Moving_Zero.java
+++ Moving_Zero.java
@@ -1,25 +1,20 @@
 public class Moving_Zero {
     public static void main(String[] args) {
         int[] a ={0,1,0,4,12}; 
-        a=zero(a);       
+        int j=0;
+        for (int i=0;i<a.length;i++){
+            if(a[i]!=0 && a[j]==0){
+                int temp = a[j];
+                a[j]=a[i];
+                a[i]=temp;
+            }
+        
+            if(a[j]!=0){
+                j++;
+            }  
+        }     
         for(int i:a){
             System.out.println(i);
         }
-    }
-     public static int[] zero(int[]a) {
-        int j=0;
-        int[] b = new int[a.length];    
-        for (int i=0;i<a.length-1;i++){
-            if(a[i]==0){
-                b[j++]=a[i];
-            }
-        }
-        for (int i=0;i<a.length-1;i++){
-            if(a[i]!=0){
-                b[j++]=a[i];
-            }
-        }
-
-        return b;
     }
 }
